91b3d40abf8d9e11b6648df3dfa00efa490854cc
emmyjarz <emmyjarz@gmail.com>
2017-07-09 00:55:32 -0700

    update all functions

--- liri.js
+++ liri.js
@@ -1,12 +1,12 @@
 var dataArr;
 var input = process.argv;
 var command = input[2];
-var userChoice = input[3];
+var userChoice = "";
 //userChoice are more than 1 word
-for (var i = 4; i < input.length; i++) {
-  userChoice = userChoice + " " + input[i];
+for (var i = 3; i < input.length; i++) {
+  userChoice += input[i] + " ";
 }
-
+// console.log(userChoice)
 function allCommands(command) {
   switch (command) {
     case "my-tweets":
@@ -29,7 +29,6 @@ function tweets() {
   var Twitter = require('twitter');
   var clientK = require("./keys.js");
   var client = new Twitter(clientK.twitterKeys);
-  // console.log(client);
   var params = {
     screen_name: "emmyjarz",
     count: 20
@@ -50,8 +49,11 @@ function spotify() {
   var spotifyK = require("./keys.js");
   var spotify = new Spotify(spotifyK.spotifyKeys);
   // console.log(spotify);
-  if (userChoice == undefined) {
+  if (userChoice == "") {
     spotify.search({
+      // type: ["track", "artist"],
+      // query: ["The Sign", "Ace of Base"],
+      //console.log the above to get result or do the below solution
       type: "track",
       query: "The Sign",
       limit: 5
@@ -62,10 +64,7 @@ function spotify() {
       for (var i = 0; i < 5; i++) {
         var song = data.tracks.items[i];
         if (song.artists[0].name == "Ace of Base") {
-          console.log(song.artists[0].name);
-          console.log(song.name);
-          console.log(song.preview_url);
-          console.log(song.album.name);
+          music(song);
         }
       }
     });
@@ -78,33 +77,41 @@ function spotify() {
         return console.log(err);
       }
       var song = data.tracks.items[0];
-      console.log(song.artists[0].name);
-      console.log(song.name);
-      console.log(song.preview_url);
-      console.log(song.album.name);
+      music(song);
     });
   }
 }
 
 function movie() {
   var request = require("request");
-  if (userChoice == undefined) {
+  if (userChoice == "") {
     userChoice = "Mr.Nobody"
   }
   userChoice = userChoice.replace(" ", "+")
   var queryUrl = "http://www.omdbapi.com/?t=" + userChoice + "&y=&plot=short&apikey=40e9cece"
   // console.log(queryUrl);
   request(queryUrl, function(err, res, body) {
+
     if (!err && res.statusCode === 200) {
       var display = JSON.parse(body);
-      console.log("Title: ", display.Title);
-      console.log("Year: ", display.Year);
-      console.log("IMDB Rating: ", display.imdbRating);
-      console.log("Rotten Tomatoes Rating: ", display.Ratings[1].Value);
-      console.log("Country: ", display.Country);
-      console.log("Language: ", display.Language);
-      console.log("Plot: ", display.Plot);
-      console.log("Actors: ", display.Actors);
+      if (display.Title == undefined) {
+        console.log("Sorry, can't find your movie, please try again!!")
+      } else {
+        console.log("Title: ", display.Title);
+        console.log("Year: ", display.Year);
+        console.log("IMDB Rating: ", display.imdbRating);
+        for (var i = 0; i < display.Ratings.length; i++) {
+          if (display.Ratings.Source === 'Rotten Tomatoes') {
+            console.log("Rotten Tomatoes Rating: ", display.Ratings[1].Value);
+          }
+        }
+        console.log("Sorry, there is no Rotten Tomatoes Rating for this moive.")
+        console.log("Country: ", display.Country);
+        console.log("Language: ", display.Language);
+        console.log("Plot: ", display.Plot);
+        console.log("Actors: ", display.Actors);
+        // console.log(display)
+      }
     }
   });
 }
@@ -120,4 +127,11 @@ function doWhat() {
     userChoice = dataArr[1].trim();
     allCommands(command);
   });
+}
+
+function music(song) {
+  console.log("Artist(s): ", song.artists[0].name);
+  console.log("Song's name: ", song.name);
+  console.log("URL: ", song.preview_url);
+  console.log("Album: ", song.album.name);
 }
